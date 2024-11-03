8cc621d37db53c2dcbc4556c09d47502ac30b443
ulisses909 <ulibicalho9@usp.br>
2024-11-03 02:51:51 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 "use strict";
 
 
-document.querySelector(".start").addEventListener
-('click', function() {
-    document.querySelector(".start").style.display = 'none';
-    console.log("GUMMI");
-})
+
+document
+  .querySelectorAll(".nome")
+  .forEach((nome) => nome.addEventListener("mousedown", mouseDown));
+
+function mouseDown(e) {
+  e.preventDefault();
+  this.addEventListener("mousemove", mouseMove);
+  this.addEventListener("mouseup", mouseUp);
+}
+
+function mouseMove(e) {
+  e.preventDefault();
+  this.style.left = e.clientX - 25 + "px";
+  this.style.top = e.clientY - 5 + "px";
+}
+
+function mouseUp(e) {
+  this.removeEventListener("mousemove", mouseMove);
+}
